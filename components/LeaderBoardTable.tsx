fbca4aa6890cd6654cb16ef46d89a92a82efb911
Julien Castets <castets.j@gmail.com>
2022-06-16 18:57:38 +0200

    Extract top three players from leaderboard

--- components/LeaderBoardTable.tsx
+++ components/LeaderBoardTable.tsx
@@ -1,37 +1,73 @@
 import styles from '../styles/LeaderBoardTable.module.css';
 import { LeaderBoardEntry } from '../lib/naseliga';
 
-type PropsType = {
-  leaderboard: LeaderBoardEntry[]
+type TopPlayersPropsTypes = {
+  first: LeaderBoardEntry
+  second: LeaderBoardEntry
+  third: LeaderBoardEntry
 }
 
-export default function LeaderBoardTable({ leaderboard }: PropsType) {
+function TopPlayers({ first, second, third } : TopPlayersPropsTypes) {
   return (
-    <div className={styles.container}>
-      <h2>Leaderboard</h2>
+    <div className={styles.topPlayers}>
+      <div className={styles.first}>
+        <div className={styles.name}>{first?.player || "?"}</div>
+        <div className={styles.score}>{first?.score || "?"}</div>
+      </div>
+
+      <div className={styles.second}>
+        <div className={styles.name}>{second?.player || "?"}</div>
+        <div className={styles.score}>{second?.score || "?"}</div>
+      </div>
+
+      <div className={styles.third}>
+        <div className={styles.name}>{third?.player || "?"}</div>
+        <div className={styles.score}>{third?.score || "?"}</div>
+      </div>
+    </div>
+  );
+}
+
+type ScoresTablePropsType = {
+  scores: LeaderBoardEntry[]
+}
+
+function ScoresTable({ scores } : ScoresTablePropsType) {
+  return (
+    <div className={styles.scoresTable}>
       <table>
         <thead>
           <tr>
-            <th></th>
             <th>Player</th>
             <th>Score</th>
           </tr>
         </thead>
         <tbody>
-          {leaderboard.map((entry, idx) =>
+          {scores.map((entry) =>
             <tr key={entry.player}>
-              <td>
-                {idx == 0 ? "🥇" :
-                 idx == 1 ? "🥈" :
-                 idx == 2 ? "🥉" :
-                 `#${idx + 1}`}
-              </td>
               <td>{entry.player}</td>
               <td>{entry.score}</td>
             </tr>
           )}
         </tbody>
       </table>
+    </div>
+  );
+}
+
+type LeaderBoardTablePropsType = {
+  leaderboard: LeaderBoardEntry[]
+}
+
+export default function LeaderBoardTable({ leaderboard }: LeaderBoardTablePropsType) {
+  const first = leaderboard.slice(0, 1)?.[0];
+  const second = leaderboard.slice(1, 2)?.[0];
+  const third = leaderboard.slice(2, 3)?.[0];
+
+  return (
+    <div className={styles.container}>
+      <TopPlayers first={first} second={second} third={third} />
+      <ScoresTable scores={leaderboard.slice(3)} />
     </div>
   );
 }
