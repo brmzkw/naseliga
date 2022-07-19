e818a6caba4f72afded8de22ab2bd274f005bda9
Julien Castets <castets.j@gmail.com>
2022-07-19 23:45:57 +0200

    Add migrations + add player.country

---
+++ prisma/migrations/20220719214421_add_player_country/migration.sql
@@ -0,0 +1,41 @@
+-- CreateTable
+CREATE TABLE "events" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR NOT NULL DEFAULT '',
+    "date" DATE NOT NULL DEFAULT CURRENT_DATE,
+
+    CONSTRAINT "events_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "players" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR NOT NULL,
+    "country" VARCHAR NOT NULL DEFAULT '',
+
+    CONSTRAINT "players_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "matches" (
+    "id" SERIAL NOT NULL,
+    "event" INTEGER NOT NULL,
+    "player_a" INTEGER NOT NULL,
+    "player_b" INTEGER NOT NULL,
+    "score_a" INTEGER NOT NULL,
+    "score_b" INTEGER NOT NULL,
+
+    CONSTRAINT "matches_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "players_name_key" ON "players"("name");
+
+-- AddForeignKey
+ALTER TABLE "matches" ADD CONSTRAINT "fk_event" FOREIGN KEY ("event") REFERENCES "events"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "matches" ADD CONSTRAINT "fk_player_a" FOREIGN KEY ("player_a") REFERENCES "players"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "matches" ADD CONSTRAINT "fk_player_b" FOREIGN KEY ("player_b") REFERENCES "players"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
