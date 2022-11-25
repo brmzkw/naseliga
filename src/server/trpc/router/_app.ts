3461fab3182fd8ce73dbacf15e5406dc010fa10d
Julien Castets <castets.j@gmail.com>
2022-11-25 17:05:18 +0100

    Comment boilerplate code

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,8 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
 import { naseligaRouter } from "./naseliga";
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
   naseliga: naseligaRouter,
 });
