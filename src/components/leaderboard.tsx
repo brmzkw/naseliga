edc2316b9f24a26f20333fad888a1c564cf0091c
Julien Castets <castets.j@gmail.com>
2022-12-02 16:54:17 +0100

    Add pulse animation when leaderboard loads

--- src/components/leaderboard.tsx
+++ src/components/leaderboard.tsx
@@ -9,17 +9,19 @@ type LeaderboardProps = {
 }
 
 const Leaderboard: React.FC<LeaderboardProps> = ({ leaderboard }) => {
+    const defaultData = Array.from(Array(10).keys()).map(() => null);
+
     return (
-        <table className="table-auto w-full">
+        <table className={`table-auto w-full ${leaderboard ? '' : 'animate-pulse'}`}>
             <tbody>
-                {leaderboard?.map((entry, idx) =>
-                    <tr key={entry.id} className="border-b border-gray-300">
+                {(leaderboard || defaultData).map((entry, idx) =>
+                    <tr key={entry?.id || idx} className="border-b border-gray-300">
                         <td className="p-3">#{idx + 1}</td>
                         <td className="p-3 flex items-center">
-                            <div className="w-5">{entry.country && <CircleFlag countryCode={entry.country.toLowerCase()} />}</div>
-                            <span className="ml-2 capitalize">{entry.name}</span>
+                            <div className="w-5">{entry?.country && <CircleFlag countryCode={entry?.country.toLowerCase()} />}</div>
+                            <span className="ml-2 capitalize">{entry?.name}</span>
                         </td>
-                        <td className="font-bold">{entry.score}</td>
+                        <td className="font-bold">{entry?.score}</td>
                     </tr>
                 )}
             </tbody>
