744f4d2ccd2b8d85b0ddb91eec5a695289e9683c
Julien Castets <castets.j@gmail.com>
2022-12-09 16:03:42 +0100

    Fix edit player form

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -68,7 +68,7 @@ const PlayersListRow: React.FC<PlayersListRowProps> = ({ player }) => {
 
     const utils = trpc.useContext();
 
-    const removeMutation = trpc.players.delete.useMutation({
+    const removePlayerMutation = trpc.players.delete.useMutation({
         onSuccess: () => {
             utils.players.invalidate();
         },
@@ -77,7 +77,7 @@ const PlayersListRow: React.FC<PlayersListRowProps> = ({ player }) => {
         },
     });
 
-    const editMutation = trpc.players.edit.useMutation({
+    const editPlayerMutation = trpc.players.edit.useMutation({
         onSuccess: () => {
             utils.players.invalidate();
             setEdit(false);
@@ -89,16 +89,16 @@ const PlayersListRow: React.FC<PlayersListRowProps> = ({ player }) => {
 
     const removePlayer = () => {
         if (confirm(`Are you sure you want to remove ${player.name}?`)) {
-            removeMutation.mutate(player);
+            removePlayerMutation.mutate(player);
         }
     };
 
-    const handleEditSubmit = ((data: PlayerFormValues) => {
-        editMutation.mutate({ id: player.id, ...data });
+    const editPlayer = ((data: PlayerFormValues) => {
+        editPlayerMutation.mutate({ id: player.id, ...data });
     });
 
     return (
-        <div className="flex gap-3 items-center pb-2 border-b">
+        <form onSubmit={handleSubmit(editPlayer)} className="flex gap-3 items-center pb-2 border-b">
             <div>
                 {
                     edit
@@ -133,34 +133,32 @@ const PlayersListRow: React.FC<PlayersListRowProps> = ({ player }) => {
             </div>
 
             <div className="flex gap-1">
-                {edit ?
-                    <form onSubmit={handleSubmit(handleEditSubmit)}>
-                        <button className="bg-green-600 rounded-md text-white text-center flex m-1 items-center p-1 pr-2" type="submit">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                {edit &&
+                    <button type="submit" className="bg-green-700 rounded-md text-white text-center flex items-center p-1 pr-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                        </svg>
+                        <div>Submit</div>
+                    </button>
+                }
+                {edit ||
+                    <>
+                        <button type="button" className="bg-slate-700 rounded-md text-white text-center flex items-center p-1 pr-2" onClick={() => setEdit(true)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-1">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
                             </svg>
-                            <div>Submit</div>
+                            <div>Edit</div>
                         </button>
-                    </form>
-                    : (
-                        <>
-                            <button className="bg-slate-700 rounded-md text-white flex -1 items-center p-1 pr-2" onClick={() => setEdit(true)}>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-1">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-                                </svg>
-                                <div>Edit</div>
-                            </button>
-                            <button className="bg-red-700 rounded-md text-white text-center flex items-center p-1 pr-2" onClick={removePlayer}>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-1">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                </svg>
-                                <div>Delete</div>
-                            </button>
-                        </>
-                    )
+                        <button type="button" className="bg-red-700 rounded-md text-white text-center flex items-center p-1 pr-2" onClick={removePlayer}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-1">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                            </svg>
+                            <div>Delete</div>
+                        </button>
+                    </>
                 }
             </div>
-        </div >
+        </form>
     );
 };
 
