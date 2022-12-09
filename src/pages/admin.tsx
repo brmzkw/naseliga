18ee7c9a293a3e6f103da30b9369256e6cfd9d73
Julien Castets <castets.j@gmail.com>
2022-12-09 16:18:25 +0100

    Move button components to components/buttons.tsx

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -11,6 +11,7 @@ import { inferRouterOutputs } from "@trpc/server";
 import { leaderboardRouter } from "../server/trpc/router/leaderboard";
 import { useForm } from "react-hook-form";
 import { playersRouter } from "../server/trpc/router/players";
+import { AddButton, EditButton, RemoveButton, SubmitButton } from "../components/buttons";
 
 
 const font = Caveat()
@@ -146,49 +147,6 @@ const PlayersListRow: React.FC<PlayersListRowProps> = ({ player }) => {
     );
 };
 
-const SubmitButton = ({ ...props }) => {
-    return (
-        <button type="submit" className="bg-green-700 rounded-md text-white text-center flex items-center p-1 pr-2" {...props}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-            </svg>
-            <div>Submit</div>
-        </button>
-    );
-};
-
-const EditButton = ({ ...props }) => {
-    return (
-        <button type="button" className="bg-slate-700 rounded-md text-white text-center flex items-center p-1 pr-2" {...props}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-1">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-            </svg>
-            <div>Edit</div>
-        </button>
-    );
-}
-
-const RemoveButton = ({ ...props }) => {
-    return (
-        <button type="button" className="bg-red-700 rounded-md text-white text-center flex items-center p-1 pr-2" {...props}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-1">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-            </svg>
-            <div>Delete</div>
-        </button>
-    );
-};
-
-const AddButton = ({ ...props }) => {
-    return (
-        <button type="button" className="bg-green-800 rounded-md text-white flex m-1 items-center p-2" {...props}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-1">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-            <div>Add</div>
-        </button>
-    );
-};
 
 const NewPlayer: React.FC = () => {
     const { register, handleSubmit, reset } = useForm<PlayerFormValues>();
