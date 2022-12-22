46fced678a7748b9b99895ea78b11a67c86ab1ef
Julien Castets <castets.j@gmail.com>
2022-12-22 22:24:31 +0100

    Create separate copmonent for PlayersList

---
+++ src/components/players-list.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import { trpc } from "../utils/trpc";
+
+import type { PlayersRouterOutput } from "../server/trpc/router/players";
+import { EditButton } from "./buttons";
+import PlayerName from "./player-name";
+import LoadingSpinner from "./loading-spinner";
+import PlayerRemoveButton from "./player-remove-button";
+import PlayerEditForm from "./player-edit-form";
+
+const PlayersList: React.FC = () => {
+    const playersQuery = trpc.players.list.useQuery();
+
+    if (!playersQuery.data) {
+        return <LoadingSpinner text="Loading players..." />;
+    }
+    return (
+        <div className="flex flex-col gap-2">
+            {playersQuery.data.map((player) =>
+                <PlayersListRow key={player.id} player={player} />
+            )}
+        </div>
+    );
+}
+
+export default PlayersList;
+
+type PlayersListRowProps = {
+    player: PlayersRouterOutput['list'][number]
+};
+
+const PlayersListRow: React.FC<PlayersListRowProps> = ({ player }) => {
+    const [edit, setEdit] = React.useState(false);
+
+    if (edit) {
+        return (
+            <PlayerEditForm player={player} onFinished={() => setEdit(false)} />
+        );
+    }
+    return (
+        <div className="flex gap-1 pb-1 border-b items-center">
+            <div className="flex-1">
+                <PlayerName player={player} />
+            </div>
+            <EditButton onClick={() => setEdit(true)} />
+            <PlayerRemoveButton player={player} />
+        </div>
+    );
+};
--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -2,34 +2,17 @@ import React from "react";
 
 import { type NextPage } from "next";
 
-import { trpc } from "../utils/trpc";
-
 import BaseLayout from "../layouts/base";
-import type { PlayersRouterOutput } from "../server/trpc/router/players";
-import { EditButton } from "../components/buttons";
-import PlayerName from "../components/player-name";
-import LoadingSpinner from "../components/loading-spinner";
 import PlayerCreateForm from "../components/player-create-form";
-import PlayerRemoveButton from "../components/player-remove-button";
-import PlayerEditForm from "../components/player-edit-form";
+import PlayersList from "../components/players-list";
 
 const AdminPage: NextPage = () => {
-    const playersQuery = trpc.players.list.useQuery();
-
-    if (!playersQuery.data) {
-        return <LoadingSpinner text="Loading players..." />;
-    }
     return (
         <BaseLayout>
             <div className="m-3">
                 <div>
                     <h2 className="font-bold mb-2">List of players</h2>
-
-                    <div className="flex flex-col gap-2">
-                        {playersQuery.data.map((player) =>
-                            <PlayersListRow key={player.id} player={player} />
-                        )}
-                    </div>
+                    <PlayersList />
 
                     <h2 className="font-bold mt-2 mb-2">Add a new player</h2>
                     <PlayerCreateForm />
@@ -40,26 +23,3 @@ const AdminPage: NextPage = () => {
 }
 
 export default AdminPage;
-
-type PlayersListRowProps = {
-    player: PlayersRouterOutput['list'][number]
-};
-
-const PlayersListRow: React.FC<PlayersListRowProps> = ({ player }) => {
-    const [edit, setEdit] = React.useState(false);
-
-    if (edit) {
-        return (
-            <PlayerEditForm player={player} onFinished={() => setEdit(false)} />
-        );
-    }
-    return (
-        <div className="flex gap-1 pb-1 border-b items-center">
-            <div className="flex-1">
-                <PlayerName player={player} />
-            </div>
-            <EditButton onClick={() => setEdit(true)} />
-            <PlayerRemoveButton player={player} />
-        </div>
-    );
-};
