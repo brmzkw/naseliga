24f37c870834a75c509bf338c7133f7937cadbe1
Julien Castets <castets.j@gmail.com>
2022-12-23 15:58:09 +0100

    Move MatchList to separate component

--- src/components/event-list.tsx
+++ src/components/event-list.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 import { useSession } from 'next-auth/react';
 
-import type { EventsRouterOutput, eventsRouter } from '../server/trpc/router/events';
+import type { eventsRouter } from '../server/trpc/router/events';
 import { trpc } from '../utils/trpc';
-import PlayerName from './player-name';
 import LoadingSpinner from './loading-spinner';
 import type { inferRouterOutputs } from '@trpc/server';
-import MatchCreateForm from './match-create-form';
-import MatchRemoveButton from './match-remove-button';
 import EventRemoveButton from './event-remove-button';
 import EventCreateForm from './event-create-form';
+import MatchList from './match-list';
 
 const EventList: React.FC = () => {
     const query = trpc.events.list.useQuery();
@@ -52,10 +50,10 @@ const Event: React.FC<EventProps> = ({ defaultOpen, event }) => {
     const { data: sessionData } = useSession();
 
     const [clicked, setClicked] = React.useState(defaultOpen);
-
     const numPlayers = [...new Set(
         event.matches.map((match) => [match.playerAId, match.playerBId]).flat()
     )].length;
+
     return (
         <>
             <div
@@ -75,7 +73,7 @@ const Event: React.FC<EventProps> = ({ defaultOpen, event }) => {
                 <h4 className="text-bold text-2xl">{event.title}</h4>
             </div>
 
-            {clicked &&
+            {clicked && (
                 <>
                     {sessionData?.user?.isAdmin && <div className="mt-3">
                         <EventRemoveButton event={event} />
@@ -83,67 +81,7 @@ const Event: React.FC<EventProps> = ({ defaultOpen, event }) => {
                     }
                     <MatchList event={event} />
                 </>
-            }
+            )}
         </>
-    );
-};
-
-type MatchListProps = {
-    event: EventsRouterOutput["list"][number],
-};
-
-const MatchList: React.FC<MatchListProps> = ({ event }) => {
-    const { data: sessionData } = useSession();
-    return (
-        <div>
-            <table className="m-auto">
-                <tbody>
-                    {event.matches.map((match) =>
-                        <tr key={match.id} className={`border-b border-purple-700 [&>td]:p-3`}>
-                            <td className={`${match.scoreA > match.scoreB ? 'font-bold' : ''}`}>
-                                <PlayerName player={match.playerA} />
-                            </td>
-                            <td className={`${match.scoreA > match.scoreB ? 'font-bold' : ''}`}>
-                                {match.scoreA}
-                                {match.ranking && (
-                                    <>
-                                        &nbsp;
-                                        <small className="text-xs font-normal">({match.ranking.rankA >= 0 ? `+${match.ranking.rankA}` : match.ranking.rankA})</small>
-                                    </>
-                                )}
-                            </td>
-
-                            <td className={`${match.scoreB > match.scoreA ? 'font-bold' : ''}`}>
-                                <PlayerName player={match.playerB} />
-                            </td>
-                            <td className={`${match.scoreB > match.scoreA ? 'font-bold' : ''}`}>
-                                {match.scoreB}
-                                {match.ranking && (
-                                    <>
-                                        &nbsp;
-                                        <small className="text-xs font-normal">({match.ranking.rankB >= 0 ? `+${match.ranking.rankB}` : match.ranking.rankB})</small>
-                                    </>
-                                )}
-                            </td>
-
-                            {sessionData?.user?.isAdmin &&
-                                <td>
-                                    <MatchRemoveButton match={match} />
-                                </td>
-                            }
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-
-            {sessionData?.user?.isAdmin &&
-                <div className="mt-4">
-                    <hr />
-                    <h4 className="text-xl text-center text-bold">Add a new match</h4>
-                    <MatchCreateForm event={event} />
-                    <hr />
-                </div>
-            }
-        </div>
     );
 };
